b78810cbb26bb761bbc8d64a970b8ad1dc208651
petch <petchportgas@gmail.com>
2018-11-14 17:27:28 +0700

    init UI

---
+++ src/Component/Splash.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { StyleSheet, Text, View } from 'react-natove'
+export default class Splash extends Component {
+    render() {
+        return (
+            <view style={style.container}>
+                <Text tyle={style.tiitle}></Text>
+            </view>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'white',
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+})
